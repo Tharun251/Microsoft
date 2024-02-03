44f58791a362a6ab3cf23cf9be63beacd9334bfb
Tharun251 <143710430+Tharun251@users.noreply.github.com>
2024-02-03 18:48:17 +0530

    Update AddUserTest.java

--- src/test/java/testScripts/AddUserTest.java
+++ src/test/java/testScripts/AddUserTest.java
@@ -8,7 +8,7 @@ import org.testng.asserts.SoftAssert;
 import genericUtilities.BaseClass;
 
 public class AddUserTest extends BaseClass {
-	//this method is used to add a new user
+	//this method is used to add a new user into the application
 
 	@Test
 	public void addUserText() {
